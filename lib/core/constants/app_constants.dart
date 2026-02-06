f9625a2d8f03553a33ef16b8824b02e5e069be1c
justin-qf <justin1998.qf@gmail.com>
2026-02-06 13:04:22 +0530

    init comite

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,4 +1,5 @@
 class AppConstants {
-  static const String googleMapsApiKey =
-      'AIzaSyDp5o-2dsM59s2wBbXyY3vU05J17dw6Qkc';
+  // static const String googleMapsApiKey =
+  //     'AIzaSyDp5o-2dsM59s2wBbXyY3vU05J17dw6Qkc';
+  static const String googleMapsApiKey = 'google-api-key';
 }
